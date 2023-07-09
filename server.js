b3ab8be541c91f58bb6d719531771ca203162396
r2pen2 <joedobbelaar@gmail.com>
2023-07-09 16:06:29 -0400

    lol

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 // Init express application
 const app = express();
 // Start listening on defined port
-app.listen(3001, () => {
-    console.log('Now listening on port ' + 3001);
+app.listen(3002, () => {
+    console.log('Now listening on port ' + 3002);
 });
 
 // Serve static files
